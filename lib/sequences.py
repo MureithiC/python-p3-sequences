bfaf65c8fb26708add5b8c2dcbaae87e74176b09
Cyrus Mureithi <cmraey@gmail.com>
2024-07-22 13:18:27 +0300

    Lab done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+	list_fib = []
+	if length > 0:
+		list_fib.append(0)
+	if length >= 2:
+		list_fib.append(1)
+		for i in range(2, length):
+			list_fib.append(list_fib[-1] + list_fib[-2])
+
+	print(list_fib)
